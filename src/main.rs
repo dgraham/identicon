be651aaa052182412a7384ae0b3af50a691a07ee
David Graham <david.malcom.graham@gmail.com>
2014-12-08 20:03:27 -0700

    Write png to stdout.

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,29 @@ use std::io;
 use std::os;
 
 use openssl::crypto::hash::{Hasher, HashType};
-use png::store_png;
+use png::write_png;
 
 use identicon::Identicon;
 
 fn main() {
-    match output() {
-        Some(path) => {
-            match hash() {
-                Some(bytes) => generate(bytes, &path),
-                None => os::set_exit_status(2),
+    match hash() {
+        Some(bytes) => {
+            match generate(bytes) {
+                Ok(_) => (),
+                Err(e) => {
+                    println!("{}", e);
+                    os::set_exit_status(2);
+                },
             }
         },
-        None => {
-            println!("Usage: identicon FILENAME INPUT");
-            os::set_exit_status(1);
-        }
+        None => os::set_exit_status(1),
     }
 }
 
-fn output() -> Option<Path> {
-    if os::args().len() == 2 {
-        let name = os::args()[1].clone();
-        Some(Path::new(name))
-    } else {
-        None
-    }
-}
-
-fn generate(input: Vec<u8>, path: &Path) {
+fn generate(input: Vec<u8>) -> Result<(), String> {
     let identicon = Identicon::new(input);
     let mut image = identicon.image();
-    let result = store_png(&mut image, path);
-    match result {
-        Ok(_) => {},
-        Err(e) => {
-            println!("{}", e);
-            os::set_exit_status(2);
-        },
-    };
+    write_png(&mut image, &mut io::stdout())
 }
 
 fn hash() -> Option<Vec<u8>> {
