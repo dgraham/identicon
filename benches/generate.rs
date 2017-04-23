02769f7c15923ec2c99b315290ee4d4a0f37641c
David Graham <david.malcom.graham@gmail.com>
2017-04-22 19:49:41 -0600

    Update digest usage in benchmark

--- benches/generate.rs
+++ benches/generate.rs
@@ -4,18 +4,18 @@ extern crate identicon;
 extern crate openssl;
 extern crate test;
 
-use openssl::crypto::hash::{hash, Type};
+use openssl::hash::{hash2, MessageDigest};
 use test::Bencher;
 
 use identicon::Identicon;
 
 #[bench]
 fn generate(x: &mut Bencher) {
-    let source = "42".to_string();
-    let input = source.as_bytes();
+    let input = "42".as_bytes();
+    let md5 = MessageDigest::md5();
 
-    x.iter(|| {
-        let bytes = hash(Type::MD5, input);
-        Identicon::new(&bytes[..]).image();
+    x.iter(|| match hash2(md5, input) {
+        Ok(bytes) => Identicon::new(&bytes).image(),
+        Err(e) => panic!(e),
     });
 }
